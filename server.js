0e66cb991d9b05e0423b434ee08d134933d21f24
Davis Bickford <davis.bickford@gmail.com>
2019-12-22 15:43:59 -0700

    more api calls added, about to start front end integration

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const database = {
 	users: [
@@ -30,7 +33,7 @@ const validate = (db, email, password) => {
 }
 
 app.get('/', (req, res) => {
-	res.send('this is working');
+	res.send(database.users);
 });
 
 app.post('/signin', (req, res) => {
@@ -39,8 +42,45 @@ app.post('/signin', (req, res) => {
 	res.json('success');
 });
 
+app.post('/register', (req, res) => {
+	const { email, name, password } = req.body;
+	bcrypt.hash(password, null, null, (err, hash) => {
+		console.log(hash);
+	})
+	database.users.push({
+		id: '125',
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date()
+	});
+	res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id', (req, res) => {
+	const { id } = req.params;
+	database.users.forEach(user => {
+		if (user.id === id) {
+			return res.json(user);
+		} 
+	})
+	return res.status(404).send('no such user');
+})
+
+app.put('/profile/update', (req, res) => {
+	const { id } = req.body;
+	database.users.forEach(user => {
+		if (user.id === id) {
+			user.entries++;
+			return res.json(user.entries);
+		} 
+	})
+	return res.status(400).send('not found');
+})
+
 app.listen(3000, ()=> {
-	console.log('app is running on port 3000');
+	console.log('app is running on port 3001');
 });
 
 
